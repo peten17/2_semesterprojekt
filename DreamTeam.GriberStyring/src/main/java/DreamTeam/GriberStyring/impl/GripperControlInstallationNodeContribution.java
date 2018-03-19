415d118a049e84307dd0b540fc8af8bf8510819c
KMeldgaard <KMeldgaard@users.noreply.github.com>
2018-03-19 12:04:35 +0100

    New node contribution

--- DreamTeam.GriberStyring/src/main/java/DreamTeam/GriberStyring/impl/GripperControlInstallationNodeContribution.java
+++ DreamTeam.GriberStyring/src/main/java/DreamTeam/GriberStyring/impl/GripperControlInstallationNodeContribution.java
@@ -12,11 +12,31 @@ import com.ur.urcap.api.ui.component.InputTextField;
  * @author Kasper_Melddgaard
  */
 public class GripperControlInstallationNodeContribution implements InstallationNodeContribution {
+    private static final String IP_KEY = "ip";
+    private static final String PORT_KEY = "port";
+    private static final String DEFAULT_IP = "000.000.000.000";
+    private static final String DEFAULT_PORT = "1";
     private DataModel model;
 
     public GripperControlInstallationNodeContribution(DataModel model) {
         this.model = model;
         
+    }
+    
+    private InputTextField ipAddress;
+    private InputTextField portNum;
+    
+    public void setIP(String ip){
+        if("".equals(ip)){
+            resetIP();
+        }else {
+            model.set(IP_KEY, ip);
+        }
+    }
+    
+    public void resetIP(){
+        ipAddress.setText(DEFAULT_IP);
+        model.set(IP_KEY, DEFAULT_IP);
     }
 
     @Override
@@ -26,7 +46,6 @@ public class GripperControlInstallationNodeContribution implements InstallationN
 
     @Override
     public void closeView() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
