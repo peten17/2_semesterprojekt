bf5a43132fbaf68b83bec726a53262f24c741bed
KMeldgaard <KMeldgaard@users.noreply.github.com>
2018-03-19 10:54:33 +0100

    new lib

--- DreamTeam.GriberStyring/src/main/java/DreamTeam/GriberStyring/impl/Activator.java
+++ DreamTeam.GriberStyring/src/main/java/DreamTeam/GriberStyring/impl/Activator.java
@@ -2,6 +2,7 @@ package DreamTeam.GriberStyring.impl;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import dk.sdu.mmmi.rd1.robotcomm.RobotClient;
 
 /**
  * Hello world activator for the OSGi bundle URCAPS contribution
@@ -9,15 +10,16 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator implements BundleActivator {
 	@Override
-	public void start(BundleContext bundleContext) throws Exception {
-		System.out.println("Activator says Hello World!");
+	public void start(final BundleContext context) throws Exception {
+		GripperControlInstallationService gripperControlInstallationservice = new GripperControlInstallationService();
                 
-                bundleContext.registerService(type, this, dctnr);
+                /*bundleContext.registerService(type, this, dctnr);*/
 	}
 
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		System.out.println("Activator says Goodbye World!");
 	}
+        
 }
 
