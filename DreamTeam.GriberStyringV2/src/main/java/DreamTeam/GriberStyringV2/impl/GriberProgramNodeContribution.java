58b441d2069d93d23849fe43bcbc40ac1f5c8280
KMeldgaard <Kasper@riber-meldgaard.dk>
2018-04-06 17:52:19 +0200

    Working URCAp GRiberstyringV2

---
+++ DreamTeam.GriberStyringV2/src/main/java/DreamTeam/GriberStyringV2/impl/GriberProgramNodeContribution.java
@@ -0,0 +1,49 @@
+package DreamTeam.GriberStyringV2.impl;
+
+import java.io.InputStream;
+
+import com.ur.urcap.api.contribution.InstallationNodeContribution;
+import com.ur.urcap.api.contribution.ProgramNodeContribution;
+import com.ur.urcap.api.domain.URCapAPI;
+import com.ur.urcap.api.domain.data.DataModel;
+import com.ur.urcap.api.domain.script.ScriptWriter;
+
+public class GriberProgramNodeContribution implements ProgramNodeContribution {
+	
+	private final DataModel model;
+	private final URCapAPI api;
+	
+	public GriberProgramNodeContribution(DataModel model, URCapAPI api){
+		this.model = model;
+		this.api = api;
+	}
+
+	@Override
+	public void openView() {
+	}
+
+	@Override
+	public void closeView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return "Griber";
+	}
+
+	@Override
+	public boolean isDefined() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void generateScript(ScriptWriter writer) {
+		// TODO Auto-generated method stub
+		writer.;
+	}
+	
+}
---
+++ DreamTeam.GriberStyringV2/src/main/java/DreamTeam/GriberStyringV2/impl/ProgramNodeService.java
@@ -0,0 +1,44 @@
+package DreamTeam.GriberStyringV2.impl;
+
+import java.io.InputStream;
+
+import com.ur.urcap.api.contribution.ProgramNodeContribution;
+import com.ur.urcap.api.domain.URCapAPI;
+import com.ur.urcap.api.domain.data.DataModel;
+
+public class ProgramNodeService implements com.ur.urcap.api.contribution.ProgramNodeService {
+	
+	public ProgramNodeService() {}
+
+	@Override
+	public String getId() {
+		return "ǴriberStyringNode";
+	}
+
+	@Override
+	public boolean isDeprecated() {
+		return false;
+	}
+
+	@Override
+	public boolean isChildrenAllowed() {
+		return true;
+	}
+
+	@Override
+	public String getTitle() {
+		return "GriberStyring";
+	}
+
+	@Override
+	public InputStream getHTML() {
+		InputStream is = this.getClass().getResourceAsStream("/html/ProgramNode.html");
+		return is;
+	}
+
+	@Override
+	public ProgramNodeContribution createNode(URCapAPI api, DataModel model) {
+		return new DreamTeam.GriberStyringV2.impl.GriberProgramNodeContribution(model, api);
+	}
+
+}
