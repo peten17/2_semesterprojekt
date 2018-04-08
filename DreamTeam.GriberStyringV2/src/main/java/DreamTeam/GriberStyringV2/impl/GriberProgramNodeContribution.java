cffc6a0ffe9ecc095b2737807de38b33eea0fe56
ur <vikpo@live.dk>
2018-04-09 01:50:15 +0200

    Cleaned up file.

--- DreamTeam.GriberStyringV2/src/main/java/DreamTeam/GriberStyringV2/impl/GriberProgramNodeContribution.java
+++ DreamTeam.GriberStyringV2/src/main/java/DreamTeam/GriberStyringV2/impl/GriberProgramNodeContribution.java
@@ -24,25 +24,25 @@ public class GriberProgramNodeContribution implements ProgramNodeContribution {
 
 	@Override
 	public void closeView() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public String getTitle() {
-		// TODO Auto-generated method stub
+		
 		return "Griber";
 	}
 
 	@Override
 	public boolean isDefined() {
-		// TODO Auto-generated method stub
+		
 		return true;
 	}
 
 	@Override
 	public void generateScript(ScriptWriter writer) {
-		// TODO Auto-generated method stub
+		
 	}
 	
 }
