2d004fa1ad5c6db80024c3b08da911a2234f0690
KMeldgaard <Kasper@riber-meldgaard.dk>
2018-04-09 13:02:26 +0200

    Working URcap sends message to external server

--- DreamTeam.GriberStyringV2/src/main/java/DreamTeam/GriberStyringV2/impl/GriberProgramNodeContribution.java
+++ DreamTeam.GriberStyringV2/src/main/java/DreamTeam/GriberStyringV2/impl/GriberProgramNodeContribution.java
@@ -2,7 +2,6 @@ package DreamTeam.GriberStyringV2.impl;
 
 import java.io.InputStream;
 
-import com.ur.urcap.api.contribution.InstallationNodeContribution;
 import com.ur.urcap.api.contribution.ProgramNodeContribution;
 import com.ur.urcap.api.domain.URCapAPI;
 import com.ur.urcap.api.domain.data.DataModel;
@@ -13,7 +12,10 @@ public class GriberProgramNodeContribution implements ProgramNodeContribution {
 	private final DataModel model;
 	private final URCapAPI api;
 	private final String popupTitle = "Popup: ";
-	private final String message = "It's a popup";
+	private final String message = "Im being executed";
+	private final String IPAdd = "10.125.45.176";
+	private final int port = 8080;
+	private final String socketName = "my_socket";
 	
 	public GriberProgramNodeContribution(DataModel model, URCapAPI api){
 		this.model = model;
@@ -47,8 +49,15 @@ public class GriberProgramNodeContribution implements ProgramNodeContribution {
 		// Create a URscript that connects to IP+Socket of RPI
 		writer.assign("message","\"" + message + "\"");
 		writer.assign("Title", "\"" + popupTitle + "\"");
-		writer.appendLine("textmsg(Title, message)");
-		writer.appendLine("");
+		writer.assign("IP", "\"" + IPAdd + "\"");
+		writer.assign("port", Integer.toString(port));
+		writer.assign("socketName", "\"" + socketName + "\"");
+		//writer.assign("command", "\"" + message + "\"");
+		writer.assign("command", "\"" + message + "\"");
+		writer.appendLine("textmsg(Title, IP)");
+		writer.appendLine("socket_open(IP, port, socketName)");
+		writer.appendLine("socket_send_string(command, socketName)");
+		writer.appendLine("socket_close(socketName)");
 		//writer.appendLine("popup(\"" + message + "\", Title, False, False, blocking=True)");
 	}
 	
